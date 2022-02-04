7b846a6876c32a4d13c901652d1e52c5e12d5e99
Steve Pikelny <spike716@gmail.com>
2022-02-03 21:55:47 -0500

    optimization + shaping

--- mapSketch3.js
+++ mapSketch3.js
@@ -6,35 +6,26 @@ function keyPressed() {
   }
 }
 
-let ellapsed = 0
 
+
+
+const BUFFER = 700
+const SCALE = 1
 let LEFT, RIGHT, TOP, BOTTOM
 function setup() {
   __canvas = createCanvas(window.innerWidth, window.innerHeight);
   noiseSeed(int(rnd(10000000000000000)))
 
-  LEFT = -width/2
-  RIGHT = width/2
-  TOP = -height/2
-  BOTTOM = height/2
-
-
-  const angle = rnd(1, 3)
-  GEN_ANGLE = () => rnd() < 0.5 ? angle : 1
-  L_ANGLE = GEN_ANGLE()
-  L_ANGLE_RND = rnd() < 0.1
-  R_ANGLE = GEN_ANGLE()
-  R_ANGLE_RND = rnd() < 0.1
+  LEFT = -width/(2*SCALE)
+  RIGHT = width/(2*SCALE)
+  TOP = -height/(2*SCALE)
+  BOTTOM = height/(2*SCALE)
 
 
 }
 
-const BUFFER = 400
-
-
 function draw() {
 
-  // resetRandomSeed()
   noLoop()
   noStroke()
   translate(width/2, height/2)
@@ -47,9 +38,15 @@ function draw() {
 
   background(0)
 
+
+
   fill(40, 10, 90)
-  scale(0.25)
-  // scale(rnd(0.25, 1))
+  scale(SCALE)
+
+  // stroke('red')
+  // fill('red')
+  // rectMode(CENTER)
+  // rect(0,0, width, height)
 
   const driftDenom = rnd() < 0.1 ? 2 : 100
 
@@ -70,87 +67,207 @@ function draw() {
 
   }
 
-  const secondaryAveCoords = primaryAveCoords.map(coord => {
+  let pBranch = 0
+  const secondaryAveCoords = primaryAveCoords.coords.map((coord) => {
     if (rnd() < 0.2) {
-      const direction = rnd() < 0.5 ? HALF_PI : PI+HALF_PI
-      return generateStreetCoords(coord[0], coord[1], coord[2] + direction)
+      const branch = pBranch++
+      const direction = rnd() < 0.5 ? 1 : -1
+      const angleAdj = direction === -1 ? HALF_PI : PI+HALF_PI
+      return generateStreetCoords(coord.x, coord.y, coord.angle + angleAdj, { direction, branch })
     }
   }).filter(exists)
 
 
-  const tertiaryAveParams = {
-    driftAmt: HALF_PI/driftDenom,
-    stopAtIntersections: [
-      primaryAveCoords,
-      ...secondaryAveCoords,
-    ]
-  }
-  const tertiaryAveCoords = secondaryAveCoords.flatMap(coords => coords.map(coord => {
+  const tertiaryAveCoords = secondaryAveCoords.flatMap((coords, i) => {
+    let sBranch = 0
+    return coords.coords.map(coord => {
+      if (rnd() < 0.5) {
+
+        const direction = rnd() < 0.5 ? 1 : -1
+        const angleAdj = direction === -1 ? HALF_PI : PI+HALF_PI
+
+        const tertiaryAveParams = {
+          direction,
+          driftAmt: HALF_PI/driftDenom,
+          stopAtIntersections: [
+            primaryAveCoords,
+            ...secondaryAveCoords,
+          ]
+        }
+        return generateStreetCoords(coord.x, coord.y, coord.angle + angleAdj, tertiaryAveParams)
+      }
+    }).filter(exists)
+  })
+
+
+  const quarternaryAveCoords = tertiaryAveCoords.flatMap(coords => coords.coords.map(coord => {
     if (rnd() < 0.5) {
-      const direction = rnd() < 0.5 ? HALF_PI : PI+HALF_PI
-      return generateStreetCoords(coord[0], coord[1], coord[2] + direction, tertiaryAveParams)
+      const direction = rnd() < 0.5 ? 1 : -1
+      const angleAdj = direction === -1 ? HALF_PI : PI+HALF_PI
+
+      const quarternaryAveParams = {
+        direction,
+        driftAmt: HALF_PI/driftDenom,
+        stopAtIntersections: [
+          primaryAveCoords,
+          ...secondaryAveCoords,
+          ...tertiaryAveCoords,
+        ]
+      }
+
+      return generateStreetCoords(coord.x, coord.y, coord.angle + angleAdj, quarternaryAveParams)
     }
   }).filter(exists))
 
-  const quarternaryAveParams = {
-    driftAmt: HALF_PI/driftDenom,
-    stopAtIntersections: [
-      primaryAveCoords,
-      ...secondaryAveCoords,
-      ...tertiaryAveCoords
-    ]
-  }
-  const quarternaryAveCoords = tertiaryAveCoords.flatMap(coords => coords.map(coord => {
-    if (rnd() < 0.5) {
-      const direction = rnd() < 0.5 ? HALF_PI : PI+HALF_PI
-      return generateStreetCoords(coord[0], coord[1], coord[2] + direction, quarternaryAveParams)
+  const streetCoords = secondaryAveCoords.flatMap(coords => coords.coords.flatMap(coord => {
+    const streetParams = {
+      driftAmt: HALF_PI/driftDenom,
+      stopAtIntersections: [
+        primaryAveCoords,
+        ...secondaryAveCoords,
+        ...tertiaryAveCoords,
+        ...quarternaryAveCoords,
+      ]
     }
-  }).filter(exists))
-
-  const streetParams = {
-    driftAmt: HALF_PI/driftDenom,
-    stopAtIntersections: [
-      primaryAveCoords,
-      ...secondaryAveCoords,
-      ...tertiaryAveCoords,
-      ...quarternaryAveCoords
+    return [
+      generateStreetCoords(coord.x, coord.y, coord.angle + HALF_PI, streetParams),
+      generateStreetCoords(coord.x, coord.y, coord.angle + HALF_PI + PI, streetParams),
     ]
-  }
-  const streetCoords = secondaryAveCoords.flatMap(coords => coords.flatMap(coord => [
-    generateStreetCoords(coord[0], coord[1], coord[2] + HALF_PI, streetParams),
-    generateStreetCoords(coord[0], coord[1], coord[2] + HALF_PI + PI, streetParams),
-  ]))
+  }))
 
 
   const START_DRAW = Date.now()
 
 
-  fill(0, 30, 80)
-  drawCoords(primaryAveCoords, (x, y) => {
-    circle(x+rnd(-0.5, 0.5), y+rnd(-0.5, 0.5), rnd(4*0.75, 5*1.2))
+  // strokeWeight(0)
+
+
+  // stroke(25)
+  // let xOffset = 215
+  // let yOffset = 215
+  // streetCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 1))
+  // quarternaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 2))
+  // tertiaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 4))
+  // secondaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 6))
+  // drawCoordsLine(primaryAveCoords.coords, xOffset, yOffset, 8)
+
+
+  // xOffset = -215
+  // yOffset = -215
+  // streetCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 1))
+  // quarternaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 2))
+  // tertiaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 4))
+  // secondaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 6))
+  // drawCoordsLine(primaryAveCoords.coords, xOffset, yOffset, 8)
+
+  // xOffset = 215
+  // yOffset = -215
+  // streetCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 1))
+  // quarternaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 2))
+  // tertiaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 4))
+  // secondaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 6))
+  // drawCoordsLine(primaryAveCoords.coords, xOffset, yOffset, 8)
+
+  // xOffset = -215
+  // yOffset = 215
+  // streetCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 1))
+  // quarternaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 2))
+  // tertiaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 4))
+  // secondaryAveCoords.forEach(coords => drawCoordsLine(coords.coords, xOffset, yOffset, 6))
+  // drawCoordsLine(primaryAveCoords.coords, xOffset, yOffset, 8)
+
+
+
+
+
+
+  const noiseCoordList = times(0, i =>
+    generateNoiseCoords(rnd(LEFT, RIGHT), rnd(TOP, BOTTOM), rnd(250, 500)),
+  )
+
+
+  const setNoiseColor = (x, y, c) => {
+    const sum = noiseCoordList.reduce((sum, nc) => inPolygon([x, y], nc) ? sum + 1 : sum, 0)
+    const h =
+      sum === 0 ? hue(c) :
+      sum === 1 ? hue(c) + 100 :
+      sum === 2 ? hue(c) + 200 :
+      hue(c) + 300
+
+    const _color = color(hfix(h), saturation(c), brightness(c))
+    fill(_color)
+    stroke(_color)
+  }
+
+
+  // stroke(color(55, 40, 100))
+  // noiseCoordList.forEach(nc => {
+  //   nc.forEach((coord, ix) => {
+  //     const [x0, y0] = coord
+  //     const [x1, y1] = nc[ix+1] || coord
+
+  //     dotLine(x0, y0, x1, y1, (x, y) => {
+  //       circle(x, y, rnd(3*0.75, 3*1.2))
+  //     })
+  //   })
+  // })
+
+
+
+  noStroke()
+
+  streetCoords.forEach(coords => drawCoords(coords.coords, (x, y, progress) => {
+    // circle(x+rnd(0, 50), y+rnd(0, 50), rnd(1*0.75, 2*1.2))
+    const isInPolygon = noiseCoordList.some(nc => inPolygon([x, y], nc))
+    setNoiseColor(x, y, color(hfix(240), 30, 80))
+
+    circle(x+rnd(-10, 10), y+rnd(-10, 10), rnd(1*0.75, 1*1.2))
+  }))
+
+
+
+
+
+
+  quarternaryAveCoords.forEach(coords => drawCoords(coords.coords, (x, y) => {
+    const isInPolygon = noiseCoordList.some(nc => inPolygon([x, y], nc))
+    setNoiseColor(x, y, color(hfix(180), 30, 80))
+
+
+    circle(x, y, rnd(2*0.75, 2*1.2))
+  }))
+
+
+
+
+  tertiaryAveCoords.forEach(coords => drawCoords(coords.coords, (x, y) => {
+    const isInPolygon = noiseCoordList.some(nc => inPolygon([x, y], nc))
+    setNoiseColor(x, y, color(hfix(120), 30, 80))
+
+    circle(x+rnd(-0.5, 0.5), y+rnd(-0.5, 0.5), rnd(2*0.75, 4*1.2))
+  }))
+
+
+
+
+  secondaryAveCoords.forEach(coords => drawCoords(coords.coords, (x, y) => {
+    const isInPolygon = noiseCoordList.some(nc => inPolygon([x, y], nc))
+    setNoiseColor(x, y, color(hfix(60), 30, 80))
+
+    circle(x+rnd(-0.5, 0.5), y+rnd(-0.5, 0.5), rnd(6*0.75, 6*1.2))
+  }))
+
+
+
+
+  drawCoords(primaryAveCoords.coords, (x, y) => {
+    const isInPolygon = noiseCoordList.some(nc => inPolygon([x, y], nc))
+    setNoiseColor(x, y, color(hfix(0), 50, 80))
+
+    circle(x+rnd(-0.5, 0.5), y+rnd(-0.5, 0.5), rnd(8*0.75, 8*1.2))
   })
 
-  fill(60, 30, 80)
-  secondaryAveCoords.forEach(coords => drawCoords(coords, (x, y) => {
-    circle(x+rnd(-0.5, 0.5), y+rnd(-0.5, 0.5), rnd(3*0.75, 4*1.2))
-  }))
 
-
-  fill(120, 30, 80)
-  tertiaryAveCoords.forEach(coords => drawCoords(coords, (x, y) => {
-    circle(x+rnd(-0.5, 0.5), y+rnd(-0.5, 0.5), rnd(3*0.75, 4*1.2))
-  }))
-
-  fill(180, 30, 80)
-  quarternaryAveCoords.forEach(coords => drawCoords(coords, (x, y) => {
-    circle(x, y, rnd(3*0.75, 4*1.2))
-  }))
-
-  fill(240, 30, 80)
-  streetCoords.forEach(coords => drawCoords(coords, (x, y, progress) => {
-    // circle(x+rnd(0, 50), y+rnd(0, 50), rnd(1*0.75, 2*1.2))
-    circle(x, y, rnd(1*0.75, 2*1.2))
-  }))
 
 
   const END = Date.now()
@@ -162,13 +279,28 @@ function draw() {
 
 
 
+const noiseXY = (cx, cy, z, maxR, noiseDivisor=1) => (progress) => {
+  const angle = progress*TWO_PI
+  const [rx, ry] = getXYRotation(angle, 1, cx + 1000, cy + 1000)
+  const r = noise(rx/noiseDivisor, ry/noiseDivisor, z/noiseDivisor) * maxR
+  return getXYRotation(angle, r, cx, cy)
+}
+
+
+function generateNoiseCoords(x, y, r) {
+  const points = 250
+  const pointFn = noiseXY(x, y, 1, r)
+  return [...times(points, p => pointFn(p/points)), pointFn(1)]
+}
+
+
 
 function drawCoords(coords, dotFn) {
   coords.forEach((coord, i) => {
+    const { x: x0, y: y0 } = coord
 
     if (i > 0) {
-      const [x0, y0] = coord
-      const [x1, y1] = coords[i-1]
+      const { x: x1, y: y1 } = coords[i-1]
 
       // strokeWeight(1)
       // stroke(255)
@@ -176,12 +308,74 @@ function drawCoords(coords, dotFn) {
       dotLine(x0, y0, x1, y1, dotFn)
 
     }
+
+    if (i === coords.length-1) {
+      push()
+      fill(255)
+      noStroke()
+      circle(x0, y0, 8)
+      pop()
+    }
   })
 }
 
+function drawCoordsLine(coords, xOffset, yOffset, weight) {
+  push()
+  strokeWeight(weight)
+  coords.forEach((coord, i) => {
+    if (i > 0) {
+      const { x: x0, y: y0 } = coord
+      const { x: x1, y: y1 } = coords[i-1]
+      line(x0+xOffset, y0+yOffset, x1+xOffset, y1+yOffset)
+    }
+  })
+  pop()
+
+}
+
+
+const coordToTuple = ({ x, y }) => [x, y]
 
 function findIntersectionPoint(c1, c2, coordLists) {
-  return coordLists.some(coordList => intersects(c1, c2, coordList[0], last(coordList)))
+  if (!coordLists.length) return false
+  const coord1 = coordToTuple(c1)
+  const coord2 = coordToTuple(c2)
+
+  return coordLists.some(coordList => {
+
+    if (coordList.coords.length < 10) return intersects(
+      coordToTuple(c1),
+      coordToTuple(c2),
+      coordToTuple(coordList.coords[0]),
+      coordToTuple(last(coordList.coords))
+    )
+
+    const size =
+      coordList.coords.length < 20 ? 2 :
+      coordList.coords.length < 40 ? 4 :
+      coordList.coords.length < 70 ? 7 :
+      10
+
+    for (let i=0; i<coordList.coords.length; i+=size) {
+      const coord3 = coordToTuple(coordList.coords[i])
+      const coord4 = coordToTuple(coordList.coords[i+size] || last(coordList.coords))
+      const bool = intersects(coord1, coord2, coord3, coord4)
+
+      if (bool) return bool
+    }
+    return false
+  })
+
+
+
+
+  // return coordLists.some(coordList => intersects(
+  //   coordToTuple(c1),
+  //   coordToTuple(c2),
+  //   coordToTuple(coordList.coords[0]),
+  //   coordToTuple(last(coordList.coords))
+  // ))
+
 }
 
 
@@ -191,6 +385,7 @@ function generateStreetCoords(startX, startY, startAngle, params={}) {
   const noiseDivisor = 5
   const blockHeight = 20
   const length = 75
+
 
   let x = startX
   let y = startY
@@ -209,11 +404,11 @@ function generateStreetCoords(startX, startY, startAngle, params={}) {
     const [nextX, nextY] = getXYRotation(angle, blockHeight, x, y)
 
     if (i) {
-      const c1 = [x, y]
-      const c2 = [nextX, nextY]
+      const c1 = { x, y }
+      const c2 = { x: nextX, y: nextY }
       const intersectionPoint = findIntersectionPoint(c1, c2, stopAtIntersections)
       if (intersectionPoint) {
-        circle(x, y, 8)
+        // circle(x, y, 8)
         break
       }
     }
@@ -221,16 +416,29 @@ function generateStreetCoords(startX, startY, startAngle, params={}) {
 
     x = nextX
     y = nextY
-    coords.push([x, y, angle])
+    coords.push({ x, y, angle })
+
+    if (
+      (x) < TOP - BUFFER ||
+      (x) > BOTTOM + BUFFER ||
+      (y) < LEFT - BUFFER ||
+      (y) > RIGHT + BUFFER
+    ) {
+      // debugger
+      break
+    }
   }
 
-  return coords
+  return {
+    coords,
+    direction: params.direction || 0,
+    branch: params.branch || 0,
+  }
 }
 
 
 function dotLine(x1, y1, x2, y2, dotFn) {
-  const d = dist(x1, y1, x2, y2)
-  const angle = atan2(x2 - x1, y2 - y1)
+  const { d, angle } = lineStats(x1, y1, x2, y2)
 
   let x = x1
   let y = y1
@@ -241,6 +449,11 @@ function dotLine(x1, y1, x2, y2, dotFn) {
   }
 }
 
+
+const lineStats = (x1, y1, x2, y2) => ({
+  d: dist(x1, y1, x2, y2),
+  angle: atan2(x2 - x1, y2 - y1)
+})
 
 
 
@@ -309,6 +522,19 @@ function curveVertexFn([x, y]) {
 
 
 
+function inPolygon(p, polygon) {
+  const infLine = [width*2, height*2]
+  const intersections = polygon.reduce((sum, l, i) => {
+    const nextI = (i+1) % polygon.length
+    const nextLine = polygon[nextI]
+
+    return intersects(p, infLine, l, nextLine)
+      ? sum + 1
+      : sum
+  }, 0)
+
+  return intersections % 2 === 1
+}
 
 function intersects(
   [x1, y1],
